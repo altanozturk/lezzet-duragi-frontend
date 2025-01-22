3a1afa397ae5d9305e6140ea98ec624263ceb5e6
altanozturk <altan317@gmail.com>
2025-01-22 14:13:39 +0300

    Update

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -86,28 +86,28 @@ function renderNavbar(username = null, isAdmin = false) {
     navbarPlaceholder.innerHTML = `
         <nav class="bg-slate-800 shadow-md h-16">
             <div class="container mx-auto px-4 text-gray-100 py-3 flex items-center h-16">
-                <a href="/src/index.html" class="flex items-center">
+                <a href="/index.html" class="flex items-center">
                     <span class="text-xl font-bold">Lezzet Durağı</span>
                 </a>
                 <div class="ml-12 flex space-x-8">
-                    <a href="/src/index.html" class="flex items-center hover:text-yellow-400 transition">
+                    <a href="/index.html" class="flex items-center hover:text-yellow-400 transition">
                         <span class="mr-2"><i class="fa-solid fa-house"></i></span>
                         Ana Sayfa
                     </a>
-                    <a href="/src/menu.html" class="flex items-center hover:text-yellow-400 transition">
+                    <a href="/menu.html" class="flex items-center hover:text-yellow-400 transition">
                         <span class="mr-2"><i class="fa-solid fa-utensils"></i></span>
                         Menü
                     </a>
-                    <a href="/src/about.html" class="flex items-center hover:text-yellow-400 transition">
+                    <a href="/about.html" class="flex items-center hover:text-yellow-400 transition">
                         <span class="mr-2"><i class="fa-solid fa-info-circle"></i></span>
                         Hakkımızda
                     </a>
-                    <a href="/src/contact.html" class="flex items-center hover:text-yellow-400 transition">
+                    <a href="/contact.html" class="flex items-center hover:text-yellow-400 transition">
                         <span class="mr-2"><i class="fa-solid fa-phone"></i></span>
                         İletişim
                     </a>
                     ${isAdmin ? `
-                        <a href="/src/admin.html" class="flex items-center hover:text-yellow-400 transition">
+                        <a href="/admin.html" class="flex items-center hover:text-yellow-400 transition">
                             <span class="mr-2"><i class="fa-solid fa-cog"></i></span>
                             Admin Paneli
                         </a>
@@ -115,12 +115,10 @@ function renderNavbar(username = null, isAdmin = false) {
                 </div>
 
                 <div class="ml-auto flex items-center space-x-6">
-                    <a href="/src/sepet.html" class="flex items-center hover:text-yellow-400 transition relative group">
+                    <a href="/sepet.html" class="flex items-center hover:text-yellow-400 transition relative group">
                         <div class="relative">
                             <i class="fa-solid fa-shopping-cart text-xl"></i>
-                            <span id="cart-count" 
-                                  class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-4 h-4 text-center text-[10px] font-bold shadow-md transform group-hover:scale-110 transition leading-4" 
-                                  style="display: none;">0</span>
+                            <span id="cart-count" class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-4 h-4 text-center text-[10px] font-bold shadow-md transform group-hover:scale-110 transition leading-4" style="display: none;">0</span>
                         </div>
                         <span class="ml-3">Sepetim</span>
                     </a>
@@ -137,7 +135,7 @@ function renderNavbar(username = null, isAdmin = false) {
                             </button>
                         </div>
                     ` : `
-                        <a href="/src/login.html" class="flex items-center hover:text-yellow-400 transition">
+                        <a href="/login.html" class="flex items-center hover:text-yellow-400 transition">
                             <i class="fa-solid fa-user mr-2"></i>
                             Giriş / Kayıt
                         </a>
